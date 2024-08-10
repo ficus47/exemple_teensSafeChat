5b6d19774b378ae14c7ac1d9c991879b68d97802
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-08-10 11:37:08 +0000

    5

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
-style="""{
-    height : 300px,
+option = st.sidebar.radio(options=["home", "speed_recontre", "chat"], label="choisissez votre mode !")
+
+if option == "home":
+    style="""{
+    height : 300px;
     color : blue
     
-}"""
+    }"""
 
-st.html(f"<h3 style={style}>{open("bienvenue.txt", "r").read()}</h3>")
+    st.html(f"<h3 style={style}>{open("bienvenue.txt", "r").read()}</h3>")
 
-option = st.sidebar.radio(options=["home", "speed_recontre", "chat"], label="choisissez votre mode !")
