c7085ad5c49c02da8666912bf93dd4262d5024a6
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-08-10 12:42:52 +0000

    12

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ elif option == "speed_rencontre":
                         """)
         st.write("""**description**:
                  je suis une petite chatte trés mignone qui ne demande que de l'amours""")
-        
+        st.button("prochaine proposition")
         
     except Exception:
         st.session_state["first"] = "no"
