dcf39f3b37fe730af474c4b8418a1635b702b3de
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-08-10 11:03:43 +0000

    454645

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.write("teenSafeChat")
 
-option = st.sidebar.selectbox(options=["home", "speed_recontre", "chat"], label="choisissez votre mode !")
+option = st.sidebar.radio(options=["home", "speed_recontre", "chat"], label="choisissez votre mode !")
