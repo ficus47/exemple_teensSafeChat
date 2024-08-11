b824a3691289a8137e87700df416617d6765f437
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-08-11 17:40:56 +0000

    27

--- main.py
+++ main.py
@@ -33,14 +33,16 @@ elif option == "speed_rencontre":
 
         st.balloons()
         
-        st.write("""**Bienvenue sur la section Speed-Date de TeenSafeChat !**\n
+        for i in """**Bienvenue sur la section Speed-Date de TeenSafeChat !**\n
                  
                     Dans cette section, vous pourrez découvrir des profils qui vous seront proposés en fonction de vos informations (âge, orientation sexuelle, etc.). Vous aurez la possibilité de les contacter ou simplement de passer au profil suivant !\n                       
                     
                     Pour vous offrir une meilleure expérience, nous utilisons votre historique de navigation. Ne vous inquiétez pas, ces informations ne seront utilisées que pour améliorer les suggestions qui vous seront faites.\n                        
                     
                     Nous basons nos recommandations sur les 50 derniers profils que vous avez consultés. Cependant, en souscrivant à notre forfait premium à seulement 0,50 € par mois, vous pourrez doubler le nombre de profils pris en compte. En plus, ce forfait vous donne accès à un algorithme de recommandation amélioré (et qui sait, peut-être bientôt basé sur l'IA 😉).\n
-                    """)
+                    """.split("\n"):
+            st.write(i)
+            
         st.button("passer au speed rencontre")
     
 elif option == "chat":
